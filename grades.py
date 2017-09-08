4f1f47ee56bbb5a9a877ec6615c5bf3eabf2d312
Noah Saso <noahsaso@gmail.com>
2017-09-08 15:23:14 -0700

    formatting

--- grades.py
+++ grades.py
@@ -517,9 +517,9 @@ def get_grade_string(grades, inDatabase, showAll):
     for c in grades:
         if c.grade >= 0.0:
             if c.grade >= 100.0:
-                grade_string = "{}: {:.1f}% [{}]".format(c.name, c.grade, c.letter)
+                grade_string = "{}: {:.1f}% [{}]".format(c.grade, c.letter, c.name)
             else:
-                grade_string = "{}: {:.2f}% [{}]".format(c.name, c.grade, c.letter)
+                grade_string = "{}: {:.2f}% [{}]".format(c.grade, c.letter, c.name)
             diff = False
             if inDatabase:
                 diff = c.diff_grade()
